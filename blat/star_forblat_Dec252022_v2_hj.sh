207619515988cbc7e782f6c0881464f5ca9994e8
hojin lee <121307215+hojinlee-98@users.noreply.github.com>
2022-12-25 02:48:53 +0900

    first commit

---
+++ blat/star_forblat_Dec252022_v2_hj.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+echo
+file=`echo $1`
+
+dir=`dirname ${file}`
+file=`basename ${file}`
+outfile=`echo "${file}" | sed 's/.txt//g'`
+
+# split file
+grep -B1 -A1 'Ref' ${dir}/${file} > ${dir}/${outfile}_star.txt
+sed '1,3d' ${dir}/${file} > ${dir}/${outfile}_line.txt
+
+grep "*" ${dir}/${file} > ${dir}/${outfile}_astar.txt
+
+while read line
+do
+	echo "   ${line}" >> ${dir}/${outfile}_star.txt
+	cat ${dir}/${outfile}_astar.txt >> ${dir}/${outfile}_star.txt
+done < ${dir}/${outfile}_line.txt
+
+rm -rf ${dir}/${outfile}_line.txt
+rm -rf ${dir}/${outfile}_astar.txt
